6c883954fcb51b8f90c37d32491fc78bdbfd5a10
Sallbertone <sallbertone@gmail.com>
2018-10-20 15:05:10 +0200

    New project. Maven dependencies. Spring and Hibernate java based configuration. Test Spring MVC Controller.

---
+++ src/main/java/mvc/config/SpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,26 @@
+package mvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer
+{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses()
+	{
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses()
+	{
+		return new Class[] {CrmAppConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings()
+	{
+		return new String[] {"/"};
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<p>It is working!</p>
+</body>
+</html>
