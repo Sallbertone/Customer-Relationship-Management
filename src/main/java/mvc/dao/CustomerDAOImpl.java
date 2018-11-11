dfe9058a087e0dbea61b036c171513b4467d902e
Sallbertone <sallbertone@gmail.com>
2018-11-11 14:33:45 +0100

    Update deleteCustomer DAO method

--- src/main/java/mvc/dao/CustomerDAOImpl.java
+++ src/main/java/mvc/dao/CustomerDAOImpl.java
@@ -8,7 +8,6 @@ import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-
 import mvc.entity.Customer;
 
 
@@ -69,8 +68,11 @@ public class CustomerDAOImpl implements CustomerDAO
 	@Override
 	public void deleteCustomer(int id)
 	{
-		getSession().createQuery("delete from Customer where id=" + id).executeUpdate();
+		Query query = getSession().createQuery("delete from Customer where id=:theId");
 		
+		query.setParameter("theId", id);
+		
+		query.executeUpdate();
 	}
 
 }
